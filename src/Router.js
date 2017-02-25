f1062d6f149bb9143421ed4c9074318772b7a67e
Chen Li <yichen.li0830@gmail.com>
2017-02-24 23:48:42 -0800

    Small UI change - platform specific UI

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import {StyleSheet, Platform} from 'react-native';
 import {Router, Scene, Actions} from 'react-native-router-flux';
 import Main from './components/Main';
 import Swiper from './components/Swiper';
 import AddCards from './components/AddCards';
 import CardList from './components/CardList';
 
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingTop: (Platform.OS === 'ios') ? 0 : 50,
+  }
+});
 
 const RouterComponent = () => {
-  return <Router>
+  return <Router hideNavBar={Platform.OS !== 'ios'}>
     <Scene key="auth" initial>
-      <Scene key="login" component={Main} title="Please Login"/>
+      <Scene key="login" component={Main} title="Login"/>
     </Scene>
   
     <Scene key="intro" component={Swiper} hideNavBar/>
