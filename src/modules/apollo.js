0ab4d14a22d0624d13a3fc7a2ce73a4615af5dd4
Chen Li <yichen.li0830@gmail.com>
2017-03-28 15:43:16 -0700

    Create expo configs for different app stages

--- src/modules/apollo.js
+++ src/modules/apollo.js
@@ -1,9 +1,21 @@
+import config from '../../exp.json';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
 
+let uri;
+switch (config.slug){
+  case "nomii-rewards-exponentjs-dev":
+    uri = "http://localhost:4000/graphql";
+    break;
+  case "nomii-rewards-exponentjs-staging":
+    uri = "https://p9s1hjsm03.execute-api.us-west-2.amazonaws.com/staging/graphql";
+    break;
+  case "nomii-rewards-exponentjs":
+    uri = "https://bnbs6szfk8.execute-api.us-west-2.amazonaws.com/dev/graphql";
+    break;
+}
 
 const networkInterface = createNetworkInterface({
-  // uri: "http://localhost:4000/graphql",
-  uri: "https://bnbs6szfk8.execute-api.us-west-2.amazonaws.com/dev/graphql",
+  uri: uri,
   opts: {
     //The "same-origin" value makes query behave similarly to
     // XMLHttpRequest with regards to cookies. Otherwise, cookies won't get sent, resulting in these
