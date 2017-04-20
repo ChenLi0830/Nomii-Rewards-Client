da6abd3a496ef8a8f11993b5eb08b1d6ee038273
Chen Li <yichen.li0830@gmail.com>
2017-04-19 21:23:57 -0700

    Recompose AppPreload and RouterWrapper

--- src/AppPreload.js
+++ src/AppPreload.js
@@ -5,126 +5,119 @@ import App from './App';
 import {Toast} from 'antd-mobile';
 import config from '../exp.json';
 import { Font } from 'expo';
+import {branch, compose, lifecycle, withHandlers, pure, renderComponent} from 'recompose';
 
+/**
+ * Purpose of this component: initialize APP - fetchUser, PreloadAssets and initAmplitude
+ * */
+const AppPreloading = (props) => {
+  // console.log("props", props);
+  return (
+      <App fbUser={props.fbUser} />
+  );
+};
 
-class AppPreloading extends React.Component {
-  state = {
-    isReady: false,
-    fbUser: undefined,
-  };
-
-  componentWillMount() {
-    (async ()=>{
-      const promises = [
-        this.fetchUser(),
-        this.cacheResourcesAsync(),
-        this.initAmplitude(),
-      ];
-
-      const result = await Promise.all(promises);
-
-      // console.log("Promise all result", result);
-
-      this.setState({
-        fbUser: result[0],
-        isReady: true,
-      });
-    })()
-  }
-
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
-
-    return (
-        <App fbUser={this.state.fbUser} />
-    );
-  }
-
-  async initAmplitude() {
-    let apiKey;
-    switch (config.slug) {
-      case "nomii-rewards-exponentjs":
-        apiKey = '803bfb08c172b8c368784b020106cfd7' // production
-        break;
-      default:
-        apiKey = 'c0e8baedac6e93c09a242c7efeedcf68' // staging
-        break;
-    }
-
-    await Amplitude.initialize(apiKey);
-    Amplitude.logEvent("Open Nomii Rewards");
-  }
-
-  async cacheResourcesAsync() {
-    const images = [
-      require('../public/images/1st-circle-check.png'),
-      require('../public/images/too-far-icon.png'),
-      require('../public/images/big-check.png'),
-      require('../public/images/promo.png'),
-      require('../public/images/insight-icon.png'),
-      require('../public/images/card-icons-onboarding.png'),
-      require('../public/images/nomii-offers-login.png'),
-      require('../public/images/Hand-over-icon-onboarding.png'),
-      require('../public/images/reward-icon-onboarding.png'),
-      require('../public/images/slogan.png'),
-      require('../public/images/circle-empty-highlight 2.png'),
-      require('../public/images/Home-empty-screen-card.png'),
-      require('../public/images/down-arrow.png'),
-      require('../public/images/DEALS2.png'),
-      require('../public/images/Promocode-placeholder.png'),
-      require('../public/images/Location-services-icon.png'),
-      require('../public/images/card-screens/1st-circle-check.png'),
-      require('../public/images/card-screens/2nd-circle-5-percent-red.png'),
-      require('../public/images/card-screens/2nd-circle-5-percent-teal.png'),
-      require('../public/images/card-screens/2nd-circle-5-percent.png'),
-      require('../public/images/card-screens/3rd-circle-orange.png'),
-      require('../public/images/card-screens/3rd-circle-teal.png'),
-      require('../public/images/card-screens/3rd-circle.png'),
-      require('../public/images/card-screens/circle-empty-highlight-orange.png'),
-      require('../public/images/card-screens/circle-empty-highlight-red.png'),
-      require('../public/images/card-screens/circle-empty-highlight-teal.png'),
-      require('../public/images/card-screens/circle-1 check.png'),
-    ];
-    for (let image of images) {
-      await Asset.fromModule(image).downloadAsync();
-    }
-    await Font.loadAsync({
-      'Avenir Next': require('../public/fonts/avenir-next-regular.ttf'),
-    });
-    return true;
-  }
-
-  async fetchUser() {
-    try {
-      // await AsyncStorage.removeItem("@NomiiStore:token");
-      const value = await AsyncStorage.getItem("@NomiiStore:token");
-      if (value !== null) {// Found token
-        // console.log(value);
-        const {token, expires} = JSON.parse(value);
-        // console.log(token, expires);
-
-        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-        const result = await response.json();
-
-        if (result.error) {
-          Amplitude.logEventWithProperties("login user with stored token - fail", {error: result.error});
-          return null;
+export default compose(
+    withHandlers({
+      initAmplitude: props => async () => {
+        let apiKey;
+        switch (config.slug) {
+          case "nomii-rewards-exponentjs":
+            apiKey = '803bfb08c172b8c368784b020106cfd7' // production
+            break;
+          default:
+            apiKey = 'c0e8baedac6e93c09a242c7efeedcf68' // staging
+            break;
         }
-        else {
-          Amplitude.setUserId(result.id);
-          Amplitude.logEvent("login user with stored token - success");
-          return {...result, token: token};// result is user with token
+  
+        await Amplitude.initialize(apiKey);
+        Amplitude.logEvent("Open Nomii Rewards");
+      },
+      cacheResourcesAsync: props => async () => {
+        const images = [
+          require('../public/images/1st-circle-check.png'),
+          require('../public/images/too-far-icon.png'),
+          require('../public/images/big-check.png'),
+          require('../public/images/promo.png'),
+          require('../public/images/insight-icon.png'),
+          require('../public/images/card-icons-onboarding.png'),
+          require('../public/images/nomii-offers-login.png'),
+          require('../public/images/Hand-over-icon-onboarding.png'),
+          require('../public/images/reward-icon-onboarding.png'),
+          require('../public/images/slogan.png'),
+          require('../public/images/circle-empty-highlight 2.png'),
+          require('../public/images/Home-empty-screen-card.png'),
+          require('../public/images/down-arrow.png'),
+          require('../public/images/DEALS2.png'),
+          require('../public/images/Promocode-placeholder.png'),
+          require('../public/images/Location-services-icon.png'),
+          require('../public/images/card-screens/1st-circle-check.png'),
+          require('../public/images/card-screens/2nd-circle-5-percent-red.png'),
+          require('../public/images/card-screens/2nd-circle-5-percent-teal.png'),
+          require('../public/images/card-screens/2nd-circle-5-percent.png'),
+          require('../public/images/card-screens/3rd-circle-orange.png'),
+          require('../public/images/card-screens/3rd-circle-teal.png'),
+          require('../public/images/card-screens/3rd-circle.png'),
+          require('../public/images/card-screens/circle-empty-highlight-orange.png'),
+          require('../public/images/card-screens/circle-empty-highlight-red.png'),
+          require('../public/images/card-screens/circle-empty-highlight-teal.png'),
+          require('../public/images/card-screens/circle-1 check.png'),
+        ];
+        for (let image of images) {
+          await Asset.fromModule(image).downloadAsync();
         }
+        await Font.loadAsync({
+          'Avenir Next': require('../public/fonts/avenir-next-regular.ttf'),
+        });
+        return true;
+      },
+      fetchUser: props => async ()=>{
+        try {
+          // await AsyncStorage.removeItem("@NomiiStore:token");
+          const value = await AsyncStorage.getItem("@NomiiStore:token");
+          if (value !== null) {// Found token
+            // console.log(value);
+            const {token, expires} = JSON.parse(value);
+            // console.log(token, expires);
+        
+            const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+            const result = await response.json();
+        
+            if (result.error) {
+              Amplitude.logEventWithProperties("login user with stored token - fail", {error: result.error});
+              return null;
+            }
+            else {
+              await Amplitude.setUserId(result.id);
+              Amplitude.logEvent("login user with stored token - success");
+              return {...result, token: token};// result is user with token
+            }
+          }
+        }
+        catch (err) {
+          console.log("err", err);
+          Amplitude.logEvent("Something went wrong (most likely network issue)");
+          Toast.fail("Something is wrong\nPlease try again", 2);
+        }
+      },
+    }),
+    lifecycle({
+      async componentWillMount(){
+        const result = await Promise.all([
+          this.props.fetchUser(),
+          this.props.cacheResourcesAsync(),
+          this.props.initAmplitude(),
+        ]);
+  
+        this.setState({
+          fbUser: result[0],
+          isReady: true,
+        });
       }
-    }
-    catch (err) {
-      console.log("err", err);
-      Amplitude.logEvent("Something went wrong (most likely network issue)");
-      Toast.fail("Something is wrong\nPlease try again", 2);
-    }
-  }
-}
-
-export default AppPreloading;
+    }),
+    branch(
+        props => !props.isReady,
+        renderComponent(AppLoading),
+    ),
+    pure,
+)(AppPreloading);
--- src/RouterWrapper.js
+++ src/RouterWrapper.js
@@ -1,145 +1,124 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Dimensions, Alert} from 'react-native';
+import React from 'react';
 import {UpsertUserMutation} from './graphql/user';
 import {graphql} from 'react-apollo';
-import {connect} from'react-redux'
+import {connect} from 'react-redux';
 import {userActions} from './modules';
-import {Amplitude, Location, Permissions} from 'expo';
+import {Location, Permissions} from 'expo';
 import Router from './Router';
+import {lifecycle, compose, withHandlers, branch, renderNothing} from 'recompose';
+import {getIfPermissionAsked} from './components/api';
 
-const styles = StyleSheet.create({
-  scrollList:{
-    marginTop: 74, flex: 1,
-  },
-  wrapper: {
-    flex: 1,
-    // paddingTop: 30,
-  },
-});
-
-class RouterWrapper extends Component{
-  state = {
-    isReady: false,
-  };
-
-  constructor(props){
-    super(props);
-    console.log("RouterWrapper props", this.props);
-  }
-
-  async upsertUser(){
-    try {
-      if (!this.props.fbUser) return null;
-
-      const {id, name, token} = this.props.fbUser;
-      console.log("id, name, token", id, name, token);
-
-      const userResult = await this.props.mutate({
-        variables: {
-          id: id,
-          fbName: name,
-          token: token,
-        }
-      });
-
-      const user = userResult.data.upsertUser;
-      console.log("user", user);
-
-      this.props.updateUser(user);
-      // this.props.updateUserId(id);
-      // console.log("user", user);
-      return user;
-
-
-    }
-    catch(err){
-      console.log("err", err);
-      // Todo handle network connection error
-    }
-  }
-
-  async getLocation(){
-    try {
-      // const locationPermission = await Permissions.getAsync(Permissions.LOCATION);
-      // console.log("locationPermission", locationPermission);
-
-      let location = {};
-  
-      // Keep track of User's location
-      const options = {
-        enableHighAccuracy: true,
-        timeInterval: 5000,
-        distanceInterval: 5
-      };
-      
-      Location.watchPositionAsync(options, (updateResult) => {
-        console.log("updateResult", updateResult);
-        this.props.updateUserLocation(updateResult.coords);
-      })
-          .catch(error=>{
-            console.log("error", error);
-            console.log("location not permitted");
-          });
-      
-      await Promise.race([
-          Location.getCurrentPositionAsync({enableHighAccuracy: true}),
-          // 2s Timeout for Android phones system version < 6
-          new Promise((resolve, reject) => setTimeout(()=>reject(new Error('timeout')), 2000)),
-      ])
-          .then(result => {
-            console.log("location", location);
-            location = result
-          })
-          .catch(error => {
-            console.log("get location timed out", error);
-          });
-
-      console.log("location", location);
-      this.props.updateUserLocation(location.coords);
-    }
-    catch(error) {
-      console.log("App doesn't have location permission");
-      console.log("error", error);
-    }
-  }
-
-  // async setAmplitudeUserId() {
-  //   try {
-  //     Amplitude.setUserId(this.props.fbUser.id)
-  //   } catch(error) {
-  //     console.log('********ERROR********')
-  //   }
-  // }
-
-  async componentWillMount(){
-    //Upsert user
-    const promises = [
-      this.getLocation(),
-      this.upsertUser(),
-      // this.setAmplitudeUserId()
-    ];
-    await Promise.all(promises);
-    this.setState({isReady: true});
-  }
-
-  render(){
-    if (!this.state.isReady) {
-      // Toast.loading('Loading...', 0);
-      return <View></View>;
-    }
-
-    return <Router/>
-  }
-}
-
-// Container
-const RouterWrapperWithMutation = graphql(UpsertUserMutation)(RouterWrapper);
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    updateUser: (user) => dispatch(userActions.updateUser(user)),
-    updateUserLocation: (location) => dispatch(userActions.updateUserLocation(location)),
-  }
+/**
+ * Pass user and location props into Redux reducer
+ * */
+const RouterWrapper = () => {
+  return <Router/>
 };
 
-export default connect(null, mapDispatchToProps)(RouterWrapperWithMutation);
+export default compose(
+    connect(
+        null,
+        {
+          updateUser: userActions.updateUser,
+          updateUserLocation: userActions.updateUserLocation,
+        }
+    ),
+    graphql(UpsertUserMutation),
+    withHandlers({
+      upsertUser: props => async() => {
+        try {
+          if (!props.fbUser) return null;
+          
+          const {id, name, token} = props.fbUser;
+          console.log("id, name, token", id, name, token);
+          
+          const userResult = await props.mutate({
+            variables: {
+              id: id,
+              fbName: name,
+              token: token,
+            }
+          });
+          
+          const user = userResult.data.upsertUser;
+          
+          props.updateUser(user);
+          return user;
+        }
+        catch (err) {
+          console.log("err", err);
+          // Todo handle network connection error
+        }
+      },
+      getLocation: props => async() => {
+        try {
+          const locationPermissionAskedBefore = await getIfPermissionAsked("location");
+          console.log("locationPermissionAskedBefore", locationPermissionAskedBefore);
+          // if (locationPermissionAskedBefore)
+          const locationPermission = await Permissions.getAsync(Permissions.LOCATION);
+          console.log("locationPermission  ", locationPermission);
+          
+          const notificationPermissionAsked = await getIfPermissionAsked("notification");
+          console.log("notificationPermissionAsked", notificationPermissionAsked);
+          const notifResponse = await Permissions.getAsync(Permissions.REMOTE_NOTIFICATIONS);
+          console.log("notification permission response ", notifResponse);
+          
+          let location = {};
+          
+          if (locationPermission.status === "granted"){
+            // Keep track of User's location
+            const options = {
+              enableHighAccuracy: true,
+              timeInterval: 5000,
+              distanceInterval: 5
+            };
+  
+            Location.watchPositionAsync(options, (updateResult) => {
+              console.log("updateResult", updateResult);
+              props.updateUserLocation(updateResult.coords);
+            })
+                .catch(error => {
+                  console.log("error", error);
+                  console.log("location not permitted");
+                });
+  
+            await Promise.race([
+              Location.getCurrentPositionAsync({enableHighAccuracy: true}),
+              // 2s Timeout for Android phones system version < 6
+              new Promise((resolve, reject) => setTimeout(() => reject(new Error('timeout')), 2000)),
+            ])
+                .then(result => {
+                  console.log("location", location);
+                  location = result
+                })
+                .catch(error => {
+                  console.log("get location timed out", error);
+                });
+          }
+          
+          console.log("location", location);
+          props.updateUserLocation(location.coords);
+        }
+        catch (error) {
+          console.log("App doesn't have location permission");
+          console.log("error", error);
+        }
+      }
+    }),
+    lifecycle({
+      async componentWillMount(){
+        //Upsert user
+        const promises = [
+          this.props.getLocation(),
+          this.props.upsertUser(),
+          // this.setAmplitudeUserId()
+        ];
+        await Promise.all(promises);
+        this.setState({isReady: true});
+      }
+    }),
+    branch(
+        props => !props.isReady,
+        renderNothing,
+    )
+)(RouterWrapper);
