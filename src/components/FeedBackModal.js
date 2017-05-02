1ccf7ebf66fada466bdb6c871978b9d3520157a8
Chen Li <yichen.li0830@gmail.com>
2017-05-01 19:48:08 -0700

    Implement FeedbackModal

--- src/components/FeedBackModal.js
+++ src/components/FeedBackModal.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import {View, Text, StyleSheet, Modal} from 'react-native';
 import {responsiveWidth, responsiveHeight} from 'react-native-responsive-dimensions';
-import {compose, withHandlers} from 'recompose';
+import {compose, withHandlers, lifecycle, branch, renderComponent} from 'recompose';
 import {connect} from 'react-redux';
 import {appActions} from '../modules';
 import ModalBox from 'react-native-modalbox';
-import Feedback1 from './Feedback1';
+import FeedbackContent1 from './FeedbackContent1';
+import {graphql} from 'react-apollo';
+import {getUserQuery} from '../graphql/user';
+import {WithInvisibleLoadingComponent} from '../components/common';
 
 const styles = StyleSheet.create({
   wrapper: {
     alignItems: "center",
     justifyContent: "center",
     width: responsiveWidth(90),
-    height: responsiveHeight(86),
-    borderRadius: 10,
+    height: responsiveHeight(90),
+    borderRadius: 5,
   },
   container: {
     flex: 1,
-    width: responsiveWidth(60),
+    width: responsiveWidth(76),
     // height: responsiveHeight(60),
   },
 });
 
 const FeedBackModal = (props) => {
+  const awaitFeedback = props.data.user.awaitFeedbacks[0];
+  console.log("props.data.user.awaitFeedbacks[0]", awaitFeedback);
+  
+  let feedBackContent;
+  props.step === 0 && (feedBackContent = <FeedbackContent1 awaitFeedback={awaitFeedback}/>);
+  props.step === 1 && (feedBackContent = <FeedbackContent1 awaitFeedback={awaitFeedback}/>);
+  
   return (
     <ModalBox isOpen={props.showModal}
               backdropPressToClose={false}
@@ -30,7 +40,7 @@ const FeedBackModal = (props) => {
               backdropOpacity={0.7}
               style={styles.wrapper}>
       <View style={styles.container}>
-        <Feedback1/>
+        {feedBackContent}
       </View>
     </ModalBox>
   )
@@ -40,14 +50,31 @@ export default compose(
     connect(
         (state) => ({
           showModal: state.app.showFeedbackModal,
-          rating: state.app.feedbackRating,
           step: state.app.feedBackStep,
+          userId: state.user.id,
         }),
         {
           toggleFeedbackModal: appActions.toggleFeedbackModal,
-          setFeedbackRating: appActions.setFeedbackRating,
           nextFeedbackStep: appActions.nextFeedbackStep,
         }
     ),
-    
+    graphql(getUserQuery, {
+      options: (props) => ({
+        variables: {id: props.userId}
+      })
+    }),
+    WithInvisibleLoadingComponent,
+    lifecycle({
+      componentDidMount() {
+        const awaitFeedbacks = this.props.data.user.awaitFeedbacks;
+        if (awaitFeedbacks.length === 0) return;
+        
+        // console.log("this.props", this.props);
+        // if (props.data)
+        // console.log(props.)
+        setTimeout(() => {
+          this.props.toggleFeedbackModal()
+        }, 1000);
+      }
+    }),
 )(FeedBackModal);
