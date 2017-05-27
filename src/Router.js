736edb7685c497da0667d06fbc8c8f803eea5310
Chen Li <yichen.li0830@gmail.com>
2017-05-26 23:02:39 -0700

    Reorganize import

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {StyleSheet, Platform, Alert, View} from 'react-native';
-import {Router, Scene, Actions} from 'react-native-router-flux';
+import {Platform, StyleSheet, View} from 'react-native';
+import {Actions, Router, Scene} from 'react-native-router-flux';
 import Login from './components/Login';
 import Swiper from './components/Swiper';
 import HomeCards from './components/HomeCards';
@@ -18,13 +18,11 @@ import SuperUserRestoVisitStats from './components/SuperUserRestoVisitStats';
 import {connect} from 'react-redux';
 import NavBarLogo from './components/NavBarLogo';
 import InputPinScreen from './components/InputPinScreen';
-import {compose, withHandlers, withState, branch, renderComponent, pure, onlyUpdateForKeys, lifecycle} from 'recompose';
-import {getIfPermissionAsked, getTimeInSec} from './components/api';
-import {Loading} from './components/common';
+import {branch, compose, onlyUpdateForKeys, renderComponent, withHandlers} from 'recompose';
+import {getTimeInSec} from './components/api';
 import {graphql} from 'react-apollo';
 import {getUserQuery} from './graphql/user';
-import {AppLoading, WithLoadingComponent} from './components/common';
-import {MainDashboard, DashboardNavBarTitle} from './components/dashboard';
+import {DashboardNavBarTitle, MainDashboard} from './components/dashboard';
 import {responsiveWidth} from 'react-native-responsive-dimensions';
 
 const styles = StyleSheet.create({
@@ -42,18 +40,18 @@ const styles = StyleSheet.create({
     height: 100,
     marginHorizontal: responsiveWidth(2),
   },
-  cardListNavBar:{
+  cardListNavBar: {
     backgroundColor: null,
     borderBottomWidth: 1,
     borderBottomColor: "#EDEDED",
     marginTop: (Platform.OS === 'ios') ? 10 : 20,
   },
-  cardListTitle:{
+  cardListTitle: {
     color: "#000",
     fontWeight: "600",
     fontSize: 16,
   },
-  navTitle:{
+  navTitle: {
     fontSize: 18,
     fontWeight: "600",
   }
@@ -66,71 +64,84 @@ const RouterComponent = (props) => {
   const {notificationPermissionAsked} = props.user;
   let {user} = props.data;
   console.log("RouterComponent user", user);
-  const isNewUser = user.lastLoginAt===user.registeredAt && getTimeInSec() - user.lastLoginAt < 60;// second part is for when user login the second time but that change is not updated to Dynamodb yet
+  const isNewUser = user.lastLoginAt === user.registeredAt &&
+      getTimeInSec() - user.lastLoginAt < 60;// second part is for when user login the second time
+                                             // but that change is not updated to Dynamodb yet
   // const locationNotGranted = !user.location;
   
   let initialScreen = props.determineInitialScreen(isNewUser, notificationPermissionAsked);
   
   const scenes = Actions.create(
       <Scene key="main">
-        <Scene key="askLocation" component = {AskLocationScreen} initial={initialScreen === "location"}
+        <Scene key="askLocation" component={AskLocationScreen}
+               initial={initialScreen === "location"}
                navigationBarStyle={styles.cardListNavBar}
-               titleStyle = {styles.cardListTitle} title="Location Services"/>
-      
+               titleStyle={styles.cardListTitle} title="Location Services"/>
+        
         <Scene key="home" component={HomeCards} type="reset" initial={initialScreen === "home"}
                hideNavBar={false} navigationBarStyle={styles.homeNavBar}
                leftButtonImage={require('../public/images/promo.png')}
-               onLeft={()=>{Actions.promoCode()}}
-               rightButtonImage={(user && (user.ownedRestaurants.length > 0 || user.isNomiiAdmin)) ? require('../public/images/insight-icon.png') : null}
-               onRight={()=>user.isNomiiAdmin ? Actions.nomiiAdminScreen() : Actions.statistics()}
-               renderTitle={() => <NavBarLogo/>} />
-      
+               onLeft={() => {
+                 Actions.promoCode()
+               }}
+               rightButtonImage={(user && (user.ownedRestaurants.length > 0 || user.isNomiiAdmin)) ?
+                   require('../public/images/insight-icon.png') : null}
+               onRight={() => user.isNomiiAdmin ? Actions.nomiiAdminScreen() : Actions.statistics()}
+               renderTitle={() => <NavBarLogo/>}/>
+        
         <Scene key="cardList" component={CardList}
                navigationBarStyle={styles.cardListNavBar}
-               titleStyle = {styles.cardListTitle}
-               title="Select Restaurant" />
-      
+               titleStyle={styles.cardListTitle}
+               title="Select Restaurant"/>
+        
         <Scene key="inputPin" component={InputPinScreen}
                navigationBarStyle={styles.cardListNavBar}
-               titleStyle = {styles.cardListTitle}
-               title="Restaurant PIN" direction="vertical" />
-      
+               titleStyle={styles.cardListTitle}
+               title="Restaurant PIN" direction="vertical"/>
+        
         <Scene key="reward" component={RewardScreen} hideNavBar
-               direction="vertical" />
-      
-        <Scene key="swiper" component={Swiper} initial={initialScreen==="swiper"} hideNavBar/>
-      
+               direction="vertical"/>
+        
+        <Scene key="swiper" component={Swiper} initial={initialScreen === "swiper"} hideNavBar/>
+        
         <Scene key="promoCode" component={PromoCode}
                direction="vertical" hideNavBar/>
         <Scene key="promoSuccess" component={PromoSuccess} direction="vertical" hideNavBar/>
-      
-        <Scene key="askNotification" component={AskNotificationScreen} hideNavBar initial={initialScreen === "askNotification"}/>
-      
+        
+        <Scene key="askNotification" component={AskNotificationScreen} hideNavBar
+               initial={initialScreen === "askNotification"}/>
+        
         <Scene key="statistics" direction="vertical" type="reset" passProps>
           <Scene key="stat" component={ShowStats} title="Report"
                  leftButtonImage={require('../public/images/close-button.png')}
                  navigationBarStyle={[styles.homeNavBar, {borderBottomWidth: 0}]}
-                 ownedRestaurant = {props.SUPickedRestaurant || user && user.ownedRestaurants[0]} titleStyle={styles.navTitle}
-                 onLeft={()=>{Actions.home()}}/>
-        
+                 ownedRestaurant={props.SUPickedRestaurant || user && user.ownedRestaurants[0]}
+                 titleStyle={styles.navTitle}
+                 onLeft={() => {
+                   Actions.home()
+                 }}/>
+          
           <Scene key="assignPin" component={AssignPIN} title="Assign PIN"
                  navigationBarStyle={styles.homeNavBar} titleStyle={styles.navTitle}/>
         </Scene>
-      
-        <Scene key="nomiiAdminScreen" component = {SuperUserScreen} navigationBarStyle={styles.homeNavBar} title="Nomii Admin"/>
-        <Scene key="nomiiAdminVisitStats" component = {SuperUserRestoVisitStats} navigationBarStyle={styles.homeNavBar} title="Nomii Admin"/>
-      
+        
+        <Scene key="nomiiAdminScreen" component={SuperUserScreen}
+               navigationBarStyle={styles.homeNavBar} title="Nomii Admin"/>
+        <Scene key="nomiiAdminVisitStats" component={SuperUserRestoVisitStats}
+               navigationBarStyle={styles.homeNavBar} title="Nomii Admin"/>
+        
         <Scene key="businessDashboard" initial navigationBarStyle={styles.dashboardNavBar}>
-          <Scene key="mainDash" component={MainDashboard} renderTitle={() => <DashboardNavBarTitle/>}
-                 ownedRestaurant = {props.SUPickedRestaurant || user && user.ownedRestaurants[0]}/>
+          <Scene key="mainDash" component={MainDashboard}
+                 renderTitle={() => <DashboardNavBarTitle/>}
+                 ownedRestaurant={props.SUPickedRestaurant || user && user.ownedRestaurants[0]}/>
         </Scene>
       </Scene>
   );
   
   return <View style={{flex: 1}}>
-        <Router scenes={scenes}/>
-        <FeedBackModal/>
-      </View>
+    <Router scenes={scenes}/>
+    <FeedBackModal/>
+  </View>
 };
 
 //Container
@@ -156,7 +167,8 @@ export default compose(
     WithLoadingComponent,
     withHandlers({
       determineInitialScreen: props => (isNewUser, notificationPermissionAsked) => {
-        console.log("isNewUser", isNewUser, "notificationPermissionAsked", notificationPermissionAsked);
+        console.log("isNewUser", isNewUser, "notificationPermissionAsked",
+            notificationPermissionAsked);
         if (isNewUser) return "swiper";
         else {
           if (!notificationPermissionAsked) return "askNotification";
